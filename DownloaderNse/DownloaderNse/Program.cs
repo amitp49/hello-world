c4da6dec4c54d55cb073c36218ec2fd3c1827168
Amit <amitp49@users.noreply.github.com>
2016-11-14 13:19:53 +0530

    Add files via upload

---
+++ DownloaderNse/DownloaderNse/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mime;
+using System.Text;
+
+namespace DownloaderNse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string remoteUri = "https://www.nseindia.com/content/historical/EQUITIES/2016/NOV/";
+            string fileName = "cm01NOV2016bhav.csv.zip", myStringWebResource = null;
+            // Create a new WebClient instance.
+            WebClient myWebClient = new WebClient();
+ 
+            // Concatenate the domain with the Web resource filename.
+            myStringWebResource = remoteUri + fileName;
+            Console.WriteLine("Downloading File \"{0}\" from \"{1}\" .......\n\n", fileName, myStringWebResource);
+            // Download the Web resource and save it into the current filesystem folder.
+            myWebClient.DownloadFile(myStringWebResource, fileName);
+            Console.WriteLine("Successfully Downloaded File \"{0}\" from \"{1}\"", fileName, myStringWebResource);
+            
+        }
+    }
+}
